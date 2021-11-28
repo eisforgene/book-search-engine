65df60c6f21e11afab654fc6f9544dbce7141901
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-11-27 18:12:48 -0800

    server

--- server/server.js
+++ server/server.js
@@ -25,6 +25,9 @@ if (process.env.NODE_ENV === 'production') {
   console.log("Dir file path:",path.join(__dirname, '../client/build'));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 // app.use(routes);
 
 db.once('open', () => {
