c4018f789dab5cf5de817d558b758e981b5bd88d
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-11-27 18:20:28 -0800

    start script changed to include client side

---
+++ client/build/precache-manifest.a58fa9629c9f1628b567f33537404a62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b68d39b149866c0bb9e04e439e68930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5aba48b42d1d19444cf",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1da24453b7f7f872dea9",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "e5aba48b42d1d19444cf",
+    "url": "/static/js/2.483854c7.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.483854c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da24453b7f7f872dea9",
+    "url": "/static/js/main.69eb9ba9.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
